23ff81787e4151154550e4c1f4b36dc376066f89
Erik Neves <erikrrn04@gmail.com>
2023-02-13 08:21:50 -0300

    fix savepoint

--- Domain/Examples/BankExamples.cs
+++ Domain/Examples/BankExamples.cs
@@ -60,13 +60,13 @@ public class BankExamples
             var money = origin.Take(amountToTransfer);
             _repository.Update(origin);
 
-            transaction.CreateSavepointAsync("Quantia removida da origem.");
+            //transaction.CreateSavepointAsync("Quantia removida da origem.");
 
             var destiny = _repository.GetById(NonExistentAccountId);
             destiny.Deposit(money);
             _repository.Update(destiny);
 
-            transaction.CreateSavepointAsync("Quantia depositada no destino.");
+            //transaction.CreateSavepointAsync("Quantia depositada no destino.");
 
             // Se chegou até aqui, todas as operações ocorreram conforme o esperado.
             transaction.Commit();
@@ -80,9 +80,8 @@ public class BankExamples
              */
 
             transaction.Rollback();
-
-            var savePoints = transaction.SupportsSavepoints;
-
+            //transaction.RollbackToSavepoint("SavePointName");
+            
             // Levantando novamente a exeção para que outras camadas da aplicação tratem o problema especifico
             //                                      notifiquem o usuário.
             throw ex;
